748bc7f14c7fc09115b1d79e861cf134179082db
Lucas-BF <lucasbf4@gmail.com>
2023-06-15 21:39:36 -0300

    V1

---
+++ Web-Art/src/main/java/com/example/demo/WebArtApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebArtApplication.class, args);
+	}
+
+}
